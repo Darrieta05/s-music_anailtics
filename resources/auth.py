981e209d42ad968af2a697065565682458808ef4
Daniel Arrieta <darrieta525@gmail.com>
2021-07-26 01:06:11 -0600

    clean up code

--- resources/auth.py
+++ resources/auth.py
@@ -9,7 +9,6 @@ from flask_restful import reqparse
 
 
 class SpotifyAuth(Resource):
-    cache_path = ".spotipyoauthcache"
     client_id = os.environ.get('SPOTIPY_CLIENT_ID')
     client_secret = os.environ.get('SPOTIPY_CLIENT_SECRET')
     redirect_uri = os.environ.get('SPOTIPY_REDIRECT_URI')
@@ -22,34 +21,30 @@ class SpotifyAuth(Resource):
         args = self.callback_parser.parse_args()
         if args["code"]:
             # we have the Authorization code!
-            post_auth_spotify_token_url = 'https://accounts.spotify.com/api/token'
             auth_str = self.client_id+':'+self.client_secret
-            b64_auth_str = base64.b64encode(auth_str.encode('ascii'))
-            body_dict = {
+            b64_auth_str_bytes = base64.b64encode(auth_str.encode('ascii'))
+            b64_auth_str = b64_auth_str_bytes.decode('ascii')
+            req_body_dict = {
                 "grant_type": "authorization_code",
                 "code": args["code"],
                 "redirect_uri": self.redirect_uri
             }
-            header_dict = {
-                # Base 64 encoded client_id:client_secret
+            req_header_dict = {
                 "Content-Type": "application/x-www-form-urlencoded",
-                "Authorization": "Basic "+b64_auth_str.decode('ascii')
+                "Authorization": "Basic " + b64_auth_str
             }
             # Send a POST request to url to get the access token
-            res = requests.post(url=post_auth_spotify_token_url,data=body_dict, headers=header_dict)
+            res = requests.post(url='https://accounts.spotify.com/api/token', data=req_body_dict, headers=req_header_dict)
             res_dict = json.loads(res.content)
-            print(res_dict["access_token"])
             session["sp_token"] = res_dict["access_token"]
             session["refresh_token"] = res_dict["refresh_token"]
-            print(session.get('sp_token'))
-
             return "Success"
+
         elif args["error"]:
-            # something happened getting the Auth code :(
+            # Something happened getting the Auth code :(
             return(args["error"])
         else:
-            # Lets get the Authorization code from Spotify:
-            auth_spotify_url = 'https://accounts.spotify.com/authorize'
+            # Let's get the Auth code from Spotify:
             dict_attr = {
                 "client_id": self.client_id,
                 "response_type": "code",
@@ -58,7 +53,6 @@ class SpotifyAuth(Resource):
                 "scope": "user-read-private playlist-read-private playlist-modify-private"
             }
             query_url = urlencode(dict_attr)
-            total_url = "{}?{}".format(auth_spotify_url, query_url)
-            print(total_url)
+            total_url = "{}?{}".format('https://accounts.spotify.com/authorize', query_url)
             return redirect(total_url)
             
